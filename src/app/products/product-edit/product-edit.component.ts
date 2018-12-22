b3484e89e0729ca8b64e5baeb43300f04a20579e
Pedro Tedim <1091234@isep.ipp.pt>
2018-12-22 22:05:14 +0000

    working on product edit component

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 import { ProductService } from '../product.service';
+import { Product } from '../product.model';
+import { MaterialFinish } from 'src/app/materialfinishes/materialfinish.model';
 
 @Component({
   selector: 'app-product-edit',
@@ -12,11 +14,12 @@ import { ProductService } from '../product.service';
 export class ProductEditComponent implements OnInit {
   id: number;
   editMode = false;
-  recipeForm: FormGroup;
+  productForm: FormGroup;
+  product: Product;
 
   constructor(private route: ActivatedRoute,
-              private productService: ProductService,
-              private router: Router) {
+    private productService: ProductService,
+    private router: Router) {
   }
 
   ngOnInit() {
@@ -30,76 +33,113 @@ export class ProductEditComponent implements OnInit {
       );*/
   }
 
-  /*onSubmit() {
-    // const newRecipe = new Recipe(
-    //   this.recipeForm.value['name'],
-    //   this.recipeForm.value['description'],
-    //   this.recipeForm.value['imagePath'],
-    //   this.recipeForm.value['ingredients']);
+  onSubmit() {
+    const newProduct = new Product(
+      this.productForm.value['productId'],
+      this.productForm.value['name'],
+      this.productForm.value['possibleMaterialFinishes'],
+      this.productForm.value['products'],
+      this.productForm.value['dimensions'],
+      this.productForm.value['category']);
     if (this.editMode) {
-      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+      this.productService.updateProduct(this.id, this.productForm.value);
     } else {
-      this.recipeService.addRecipe(this.recipeForm.value);
+      this.productService.addProduct(this.productForm.value);
     }
     this.onCancel();
   }
-
-  onAddIngredient() {
-    (<FormArray>this.recipeForm.get('ingredients')).push(
-      new FormGroup({
-        'name': new FormControl(null, Validators.required),
-        'amount': new FormControl(null, [
-          Validators.required,
-          Validators.pattern(/^[1-9]+[0-9]*$/)
-        ])
-      })
-    );
-  }
-
-  onDeleteIngredient(index: number) {
-    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
-  }
+  /*
+    onAddSubProduct() {
+      (<FormArray>this.recipeForm.get('ingredients')).push(
+        new FormGroup({
+          'name': new FormControl(null, Validators.required),
+          'amount': new FormControl(null, [
+            Validators.required,
+            Validators.pattern(/^[1-9]+[0-9]*$/)
+          ])
+        })
+      );
+    }
+  
+    onDeleteSubProduct(index: number) {
+      (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+    }*/
 
   onCancel() {
-    this.router.navigate(['../'], {relativeTo: this.route});
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
   getControls() {
-    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+    return (<FormArray>this.productForm.get('products')).controls,
+      (<FormArray>this.productForm.get('possibleMaterialFinishes')).controls;
   }
 
   private initForm() {
-    let recipeName = '';
-    let recipeImagePath = '';
-    let recipeDescription = '';
-    let recipeIngredients = new FormArray([]);
+    let productId = null;
+    let productName = '';
+    let productProducts = null;
+    let productPossibleMaterialFinishes = null;
+    let productDimensionminHeight = null;
+    let productDimensionmaxHeight = null;
+    let productDimensionminDepth = null;
+    let productDimensionmaxDepth = null;
+    let productDimensionminWidth = null;
+    let productDimensionmaxWidth = null;
+    let productCategory= null;
 
     if (this.editMode) {
-      const recipe = this.recipeService.getRecipe(this.id);
-      recipeName = recipe.name;
-      recipeImagePath = recipe.imagePath;
-      recipeDescription = recipe.description;
-      if (recipe['ingredients']) {
-        for (let ingredient of recipe.ingredients) {
-          recipeIngredients.push(
+      this.productService.getProduct(this.id)
+        .then((product) => {
+          this.product = product;
+          console.log(this.product);
+        });
+      productName = this.product.name;
+      productId = this.product.productId;
+      productDimensionminHeight = this.product.dimensions.minHeight;
+      productDimensionmaxHeight = this.product.dimensions.maxHeight;
+      productDimensionminDepth = this.product.dimensions.minDepth;
+      productDimensionmaxDepth = this.product.dimensions.maxDepth;
+      productDimensionminWidth = this.product.dimensions.minWidth;
+      productDimensionmaxWidth = this.product.dimensions.maxWidth;
+      productCategory
+
+      if (this.product['products']) {
+        for (let product of this.product.products) {
+          productProducts.push(
             new FormGroup({
-              'name': new FormControl(ingredient.name, Validators.required),
-              'amount': new FormControl(ingredient.amount, [
-                Validators.required,
-                Validators.pattern(/^[1-9]+[0-9]*$/)
-              ])
+              'name': new FormControl(product.name, Validators.required),
+              'id': new FormControl(product.productId, Validators.required)
+            })
+          );
+        }
+      }
+
+      if (this.product['possibleMaterialFinishes']) {
+        for (let materialFinish of this.product.possibleMaterialFinishes) {
+          productPossibleMaterialFinishes.push(
+            new FormGroup({
+              'name': new FormControl(materialFinish.name, Validators.required),
+              'material': new FormControl(materialFinish.material, Validators.required)
             })
           );
         }
       }
     }
 
-    this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName, Validators.required),
-      'imagePath': new FormControl(recipeImagePath, Validators.required),
-      'description': new FormControl(recipeDescription, Validators.required),
-      'ingredients': recipeIngredients
+    this.productForm = new FormGroup({
+      'productId': new FormControl(productId, Validators.required),
+      'name': new FormControl(productName, Validators.required),
+      'productPossibleMaterialFinishes': new FormControl(productPossibleMaterialFinishes, Validators.required),
+      'products': new FormControl(productProducts, Validators.required),
+      'productDimensionminHeight': new FormControl(productDimensionminHeight, Validators.required),
+      'productDimensionmaxHeight': new FormControl(productDimensionmaxHeight, Validators.required),
+      'productDimensionminDepth': new FormControl(productDimensionminDepth, Validators.required),
+      'productDimensionmaxDepth': new FormControl(productDimensionmaxDepth, Validators.required),
+      'productDimensionminWidth': new FormControl(productDimensionminWidth, Validators.required),
+      'productDimensionmaxWidth': new FormControl(productDimensionmaxWidth, Validators.required),
+      'productCategory': new FormControl(productCategory, Validators.required),
+
     });
-  }*/
+  }
 
 }
