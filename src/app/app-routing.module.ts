ff379372af1c6cb0c9b00392bf27e9b8aad6c292
RodrigoSoares <1140420@isep.ipp.pt>
2018-12-22 20:17:37 +0000

    UC32 - SGRAI module. Managed to integrate a demo Three.js project into our Angular front-end. Next step is to integrate our custom closet project from SGRAI - refs issue #55

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { HomeComponent } from './core/home/home.component';
+import { EngineComponent } from './engine/engine.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', loadChildren: './products/products.module#ProductsModule'},
-  { path: 'shopping-list', component: ShoppingListComponent }
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'engine', component: EngineComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module'
 import { CoreModule } from './core/core.module';
+import { EngineComponent } from './engine/engine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EngineComponent,
   ],
   imports: [
     BrowserModule,
