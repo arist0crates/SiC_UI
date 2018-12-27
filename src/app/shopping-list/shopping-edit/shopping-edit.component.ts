ef4f5d2a9c2629cd0daa21ebcfd9cf2f0a8377fe
Pedro Tedim <1091234@isep.ipp.pt>
2018-12-27 11:47:43 +0000

    Customer can now add more subproducts to a product

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -10,6 +10,9 @@ import { Subscription } from 'rxjs/Subscription';
 import { ShoppingListService } from '../shopping-list.service';
 import { Product } from 'src/app/products/product.model';
 import { ValueTransformer } from '@angular/compiler/src/util';
+import { ProductService } from 'src/app/products/product.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductsComponent } from 'src/app/products/products.component';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -26,10 +29,21 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   heightSliderVal: number;
   depthSliderVal: number;
   widthSliderVal: number;
+  products: Product[];
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService, private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(() => {
+        this.productService.getProducts()
+          .then((products) => {
+            this.products = products;
+            console.log(this.products);
+          })
+      })
     this.subscription = this.slService.startedEditing
       .subscribe(
         (index: number) => {
@@ -37,9 +51,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
           this.editMode = true;
           this.editedItem = this.slService.getProduct(index);
           this.finalProduct = this.slService.getProduct(index);
-          this.heightSliderVal =this.editedItem.dimensions.minHeight;
-          this.depthSliderVal =this.editedItem.dimensions.minDepth;
-          this.widthSliderVal =this.editedItem.dimensions.minWidth;
+          this.heightSliderVal = this.editedItem.dimensions.minHeight;
+          this.depthSliderVal = this.editedItem.dimensions.minDepth;
+          this.widthSliderVal = this.editedItem.dimensions.minWidth;
           this.slForm.setValue({
             name: this.editedItem.name,
           })
@@ -48,11 +62,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onSubmit(form: NgForm) {
-    this.editedItem.dimensions.minHeight= this.heightSliderVal;
-    this.editedItem.dimensions.minDepth= this.depthSliderVal;
-    this.editedItem.dimensions.minWidth= this.widthSliderVal;
+    this.editedItem.dimensions.minHeight = this.heightSliderVal;
+    this.editedItem.dimensions.minDepth = this.depthSliderVal;
+    this.editedItem.dimensions.minWidth = this.widthSliderVal;
     const value = form.value;
-    console.log("NAME:"+ value.name);
+    console.log("NAME:" + value.name);
     const newProduct = new Product(value.name, this.editedItem.productId, this.editedItem.possibleMaterialFinishes, this.editedItem.products, this.editedItem.dimensions, this.editedItem.category);
     console.log(newProduct.name);
     if (this.editMode) {
@@ -79,7 +93,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onDeleteSubProduct(index: number) {
-    this.finalProduct.products.splice(index, 1);
+    this.editedItem.products.splice(index, 1);
+  }
+  onAddSubProduct(index: number) {
+    this.editedItem.products.push(this.products[index]);
   }
 
   onHeightSliderChange(val) {
